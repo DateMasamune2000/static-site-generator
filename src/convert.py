e8fc2bc06b72b93867a2895dd7933db3b2211c48
Varun Girimaji <vgirimaji@gmail.com>
2026-05-10 15:21:03 -0500

    Chapter 3, lesson 2

--- src/convert.py
+++ src/convert.py
@@ -7,7 +7,7 @@ text_node_vals = {
     TextType.ITALIC: 'i',
     TextType.CODE: 'code'
 }
-    
+
 def text_node_to_html_node(text_node):
     if text_node.text_type in text_node_vals:
         tag = text_node_vals[text_node.text_type]
@@ -18,3 +18,23 @@ def text_node_to_html_node(text_node):
         return LeafNode(tag="img", value="", props={"src":text_node.url,"alt":text_node.text})
         
     return LeafNode(tag=tag, value=value, props=props)
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+	new_nodes = []
+
+	for old_node in old_nodes:
+		if old_node.text_type != TextType.PLAIN:
+			new_nodes.append(old_node)
+
+		l = old_node.text.split(delimiter)
+
+		if len(l) % 2 == 0:
+			raise("Invalid Markdown syntax")
+
+		for i, x in enumerate(l):
+			if i % 2 == 0:
+				new_nodes.append(TextNode(x, TextType.PLAIN))
+			else:
+				new_nodes.append(TextNode(x, text_type))
+
+	return new_nodes
---
+++ src/test_split_nodes_delimiter.py
@@ -0,0 +1,77 @@
+import unittest
+
+from textnode import TextNode, TextType
+from convert import split_nodes_delimiter
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+	def test_plain(self):
+		plain = TextNode(text_type=TextType.PLAIN, text="hello, world")
+
+		nodes_bold = split_nodes_delimiter([plain], "**", TextType.BOLD)
+		self.assertEqual(nodes_bold, [TextNode("hello, world", TextType.PLAIN)])
+
+		nodes_bold = split_nodes_delimiter([plain], "_", TextType.ITALIC)
+		self.assertEqual(nodes_bold, [TextNode("hello, world", TextType.PLAIN)])
+
+		nodes_bold = split_nodes_delimiter([plain], "`", TextType.CODE)
+		self.assertEqual(nodes_bold, [TextNode("hello, world", TextType.PLAIN)])
+
+	def test_bold(self):
+		bold = TextNode(text_type=TextType.PLAIN, text="hello, **world**!")
+
+		nodes_bold = split_nodes_delimiter([bold], "**", TextType.BOLD)
+		self.assertEqual(nodes_bold, [
+			TextNode("hello, ", TextType.PLAIN),
+			TextNode("world", TextType.BOLD),
+			TextNode("!", TextType.PLAIN)
+		])
+
+		nodes_italic = split_nodes_delimiter([bold], "_", TextType.ITALIC)
+		self.assertEqual(nodes_italic, [
+			TextNode("hello, **world**!", TextType.PLAIN)
+		])
+
+		nodes_code = split_nodes_delimiter([bold], "`", TextType.CODE)
+		self.assertEqual(nodes_code, [
+			TextNode("hello, **world**!", TextType.PLAIN)
+		])
+
+	def test_italic(self):
+		italic = TextNode(text_type=TextType.PLAIN, text="hello, _world_!")
+
+		nodes_italic = split_nodes_delimiter([italic], "_", TextType.ITALIC)
+		self.assertEqual(nodes_italic, [
+			TextNode("hello, ", TextType.PLAIN),
+			TextNode("world", TextType.ITALIC),
+			TextNode("!", TextType.PLAIN)
+		])
+
+		nodes_bold = split_nodes_delimiter([italic], "**", TextType.BOLD)
+		self.assertEqual(nodes_bold, [
+			TextNode("hello, _world_!", TextType.PLAIN)
+		])
+
+		nodes_code = split_nodes_delimiter([italic], "`", TextType.CODE)
+		self.assertEqual(nodes_code, [
+			TextNode("hello, _world_!", TextType.PLAIN)
+		])
+
+	def test_code(self):
+		code = TextNode(text_type=TextType.PLAIN, text="hello, `world`!")
+
+		nodes_code = split_nodes_delimiter([code], "`", TextType.CODE)
+		self.assertEqual(nodes_code, [
+			TextNode("hello, ", TextType.PLAIN),
+			TextNode("world", TextType.CODE),
+			TextNode("!", TextType.PLAIN)
+		])
+
+		nodes_bold = split_nodes_delimiter([code], "**", TextType.BOLD)
+		self.assertEqual(nodes_bold, [
+			TextNode("hello, `world`!", TextType.PLAIN)
+		])
+
+		nodes_italic = split_nodes_delimiter([code], "_", TextType.ITALIC)
+		self.assertEqual(nodes_italic, [
+			TextNode("hello, `world`!", TextType.PLAIN)
+		])
